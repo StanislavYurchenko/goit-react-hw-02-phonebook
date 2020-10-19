631bc9760a0bb446e8467ce605d097a1a68d7e20
Stas Yurchenko <yurchenko.stanislav@ukr.net>
2020-10-19 23:32:27 +0300

    Updates

---
+++ precache-manifest.18a80881ec178865569e68818b1c9169.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b98da5c4080e87d6cde41396c69b6409",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "6600ffcda473d84dee18",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.9531bbfb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.9531bbfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84cebcdce01f9fd65373",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.a7e066a7.chunk.js"
+  },
+  {
+    "revision": "65c8a2f1bf8ffcc01bde",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.d8bf734b.js"
+  }
+]);
