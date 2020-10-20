0bf3c505ae4936920f80c7a4a61877cb87fcbc44
Stas Yurchenko <yurchenko.stanislav@ukr.net>
2020-10-20 20:46:05 +0300

    ready

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { Button, ItemContact, ListContact } from './styles.js';
 
 function ContactList({ contacts, filter, onRemove }) {
-  const markupItem = ({id, name, number}, onRemove) => (
+  const markupItem = ({ id, name, number }, onRemove) => (
     <ItemContact key={id}>
       <span>{name}: {number}</span>
       <Button onClick={() => onRemove(id)}>remove</Button>
     </ItemContact>
   );
   
-  return (
+  return !!contacts.length && (
     <ListContact>
       {
-        filter  
+        filter
           ? contacts
             .filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()))
             .map(element => markupItem(element, onRemove))
